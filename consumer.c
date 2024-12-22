13095f479ee4569f134a1cdd8fd4de7d4aef2b68
Martin Laubscher <martin.laubscher@me.com>
2024-12-22 12:24:00 +0100

    consumer no longer calling recv to avoid potential block

--- consumer.c
+++ consumer.c
@@ -15,9 +15,7 @@
 #define min(a,b) (((a) < (b)) ? (a) : (b))
 
 void *consumer_thread(void *arg) {
-
-    char recv_buf[MAX_MSG_SIZE];
-    char send_buf[LEN_PREFIX_SIZE + MAX_MSG_SIZE];
+    // char recv_buf[MAX_MSG_SIZE];
 
     const ConsumerArgs *consumer_args = (ConsumerArgs *) arg;
     Queue *queue = consumer_args->queue;
@@ -34,19 +32,18 @@ void *consumer_thread(void *arg) {
     }
 
     while (1) {
-
         char *msg = peek_queue(queue);
         if (strcmp(msg, "quit") == 0) {
             break;
         }
 
-        const u_int32_t msg_len = min(strlen(msg), MAX_MSG_SIZE - 1);
+        const uint32_t msg_len = min(strlen(msg), MAX_MSG_SIZE - 1);
         const uint32_t network_msg_len = htonl(msg_len);
         size_t total_len = sizeof(network_msg_len) + msg_len;
+        char send_buf[total_len];
 
         memcpy(send_buf, &network_msg_len, sizeof(network_msg_len));
         memcpy(send_buf + sizeof(network_msg_len), msg, msg_len);
-        // send_buf[total_len] = '\0';
 
         if (send_all(sockfd, send_buf, &total_len) == -1) {
             perror("send failed: message\n");
@@ -58,18 +55,16 @@ void *consumer_thread(void *arg) {
         printf("sent\t\t'%s'\n", msg);
         free(dequeue(queue));
 
-        long numbytes = recv(sockfd, recv_buf, MAX_MSG_SIZE - 1, MSG_NOSIGNAL);
-        if (numbytes == -1) {
-            perror("recv failed");
-            close(sockfd);
-            sockfd = connect_to_server(hostname, port); // try reconnecting on error
-            continue;
-        }
-
-        recv_buf[numbytes] = '\0';
-        printf("received\t'%s'\n\n", recv_buf);
-        // sleep(1);
-
+        // long numbytes = recv(sockfd, recv_buf, MAX_MSG_SIZE - 1, MSG_NOSIGNAL);
+        // if (numbytes == -1) {
+        //     perror("recv failed");
+        //     close(sockfd);
+        //     sockfd = connect_to_server(hostname, port); // try reconnecting on error
+        //     continue;
+        // }
+        //
+        // recv_buf[numbytes] = '\0';
+        // printf("received\t'%s'\n\n", recv_buf);
     }
 
     close(sockfd);
